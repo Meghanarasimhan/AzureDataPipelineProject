2600abedfe63843fab250d2c569bf67eb1422f85
Meghanarasimhan <Meghanarasimhan>
2025-07-10 23:53:12 +0000

    Master Transformation

--- config.py
+++ config.py
@@ -3,6 +3,12 @@ class OlympicConfig:
     MEDAL_EFFICIENCY_HIGH = 0.30
     MEDAL_EFFICIENCY_MEDIUM = 0.15
     SPECIALIZATION_THRESHOLD = 25.0
+    FEMALE_DOMINATED_THRESHOLD = 70.0    # >70% female = female dominated
+    MALE_DOMINATED_THRESHOLD = 70.0      # >70% male = male dominated  
+    BALANCED_SPORT_MIN = 40.0            # 40-60% = balanced
+    BALANCED_SPORT_MAX = 60.0
+    LARGE_SPORT_TOP_N = 10               # Top 10 sports = "large sports"
+    MEDIUM_SPORT_TOP_N = 20  
     HIGH_RISK_THRESHOLD = 50.0
     FOCUSED_STRATEGY_THRESHOLD = 30.0
     TOP_N_RESULTS = 15
